8e52670bea01c788a0e2865902cf7e4a947b5f1a
Lianne Dakanno <liannedakanno@gmail.com>
2023-06-14 07:27:00 -0500

    commit tree b5607a6cd0b47bfe276d2668c469ddbbef2376ac

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 30, "M", "Black", "Cute", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 30, "M", "Brown", "Lazy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 30, "F", "Black", "Lazy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 30, "M", "Calm", "Cute", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 30, "F", "Brown", "Calm", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 30, "M", "Brown", "Grouchy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 30, "F", "Brown", "Grouchy", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 30, "M", "Black", "Calm", true);
+
